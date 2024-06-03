eb4be5bb3e4734e1c1bd75ba0a957b7d17c42c35
Peter Winkelmans <peter.winkelmans@sennit.be>
2024-06-03 23:35:36 +0200

    Fourth commit

--- main.go
+++ main.go
@@ -5,9 +5,7 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -32,21 +30,7 @@ func main() {
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-
-	router.Run()
+	router.Run("localhost:8080")
 }
 
 // getAlbums responds with the list of all albums as JSON.
